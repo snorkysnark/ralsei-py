24b9529b6a66328fed9c16d75df1f1ccccb6b001
francisthebasilisk <snorkysnark@gmail.com>
2022-07-11 15:41:13 +0300

    comments in map_fn module

--- ralsei/map_fn/protocols.py
+++ ralsei/map_fn/protocols.py
@@ -2,10 +2,12 @@ from typing import Any, Generator, Protocol
 
 
 class OneToOne(Protocol):
+    """Any function of type (*args) -> dict"""
     def __call__(self, *args: Any) -> dict:
         ...
 
 
 class OneToMany(Protocol):
+    """Any function of type (*args) -> Generator[dict]"""
     def __call__(self, *args: Any) -> Generator[dict, None, None]:
         ...
