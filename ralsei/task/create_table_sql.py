b2c693035b3b6e2931cc7c699d3ee0bbbcf8b5ab
snorkysnark <snorkysnark@gmail.com>
2023-08-16 22:04:19 +0200

    fix create_table_sql
    remove context experiments

--- ralsei/task/create_table_sql.py
+++ ralsei/task/create_table_sql.py
@@ -3,8 +3,10 @@ from ralsei.task.context import MultiConnection
 
 from ralsei.templates import Table
 from ralsei import dict_utils
-from ralsei.templates.renderer import RalseiRenderer
+from ralsei.templates import RalseiRenderer, DEFAULT_RENDERER
 from .task import Task
+
+DROP_TABLE = "DROP TABLE IF EXISTS {{ table }};"
 
 
 class CreateTableSql(Task):
@@ -14,6 +16,7 @@ class CreateTableSql(Task):
         self,
         sql: str,
         table: Table,
+        renderer: RalseiRenderer = DEFAULT_RENDERER,
         params: dict = {},
     ) -> None:
         """Args:
@@ -25,24 +28,18 @@ class CreateTableSql(Task):
         - renderer (RalseiRenderer, optional): Environment for rendering the templates
         - params (dict, optional): Extra parameters given the the `sql` template"""
 
-        self.__params = dict_utils.merge_no_dup({"table": table}, params)
-        self.__raw_sql = sql
-        # self.sql = renderer.render(sql, jinja_args)
-        # self.drop_sql = renderer.render(DROP_TABLE, jinja_args)
+        jinja_args = dict_utils.merge_no_dup({"table": table}, params)
 
-    def render(self, renderer: RalseiRenderer) -> None:
-        self.__sql = renderer.render(self.__raw_sql, self.__params)
-        self.__drop_sql = renderer.render(
-            "DROP TABLE IF EXISTS {{ table }};", self.__params
-        )
+        self.sql = renderer.render(sql, jinja_args)
+        self.drop_sql = renderer.render(DROP_TABLE, jinja_args)
 
     def run(self, conn: MultiConnection) -> None:
         with conn.pg().cursor() as curs:
-            curs.execute(self.__sql)
+            curs.execute(self.sql)
 
     def delete(self, conn: MultiConnection) -> None:
         with conn.pg().cursor() as curs:
-            curs.execute(self.__drop_sql)
+            curs.execute(self.drop_sql)
 
     def get_sql_scripts(self) -> dict[str, Composable]:
-        return {"Create": self.__sql, "Drop": self.__drop_sql}
+        return {"Create": self.sql, "Drop": self.drop_sql}
