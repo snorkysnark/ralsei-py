d5dfb95641bddd2218f5c88a594c00506468421a
snorkysnark <snorkysnark@gmail.com>
2023-08-18 22:37:25 +0200

    export TaskDefinitions
    for writing the factory's return type

--- ralsei/__init__.py
+++ ralsei/__init__.py
@@ -6,7 +6,7 @@ from .task import (
     MapToNewTable,
     MapToNewColumns,
 )
-from .cli import RalseiCli
+from .cli import RalseiCli, TaskDefinitions
 from .map_fn import FnBuilder, GeneratorBuilder
 from .cursor_factory import ServerCursorFactory
 from .context import PsycopgConn
