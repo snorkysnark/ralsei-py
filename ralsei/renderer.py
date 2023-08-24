0b32773ab55ba07024e41d7151fe89464357de56
snorkysnark <snorkysnark@gmail.com>
2023-08-24 23:21:10 +0200

    try document 'renderer' module

--- ralsei/renderer.py
+++ ralsei/renderer.py
@@ -4,6 +4,24 @@ from .templates import Column
 
 
 class RalseiRenderer(JinjaPsycopg):
+    """
+    JinjaPsycopg renderer pre-initialized with ralsei's environment variables
+
+    ## Usage
+
+    Build template:
+    ```python
+    renderer.from_string("SELECT * FROM {{table}}")
+    # jinja_psycopg.SqlTemplate
+    ```
+
+    Render SQL:
+    ```python
+    renderer.render("SELECT * FROM {{table}}", {"table": Table("foo")})
+    # psycopg.sql.Composed
+    ```
+    """
+
     def _prepare_environment(self):
         super()._prepare_environment()
 
