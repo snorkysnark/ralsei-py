0bc577659ffc2651ce7401c743e6598b2d2b5935
snorkysnark <snorkysnark@gmail.com>
2023-08-28 17:05:57 +0200

    test MapToNewColumns

---
+++ tests/test_map_to_new_columns.py
@@ -0,0 +1,97 @@
+from psycopg.sql import Identifier
+import pytest
+from ralsei import (
+    PsycopgConn,
+    Table,
+    MapToNewColumns,
+    ValueColumn,
+    FnBuilder,
+    DEFAULT_RENDERER,
+)
+from common.db_helper import get_rows
+
+
+def test_map_columns(conn: PsycopgConn):
+    def double(val: int):
+        return {"doubled": val * 2}
+
+    table = Table("test_map_columns")
+    conn.pg().execute(
+        DEFAULT_RENDERER.render(
+            """\
+            CREATE TABLE {{table}}(
+                id SERIAL PRIMARY KEY,
+                val INT
+            );
+            INSERT INTO {{table}}(val) VALUES
+            (2),(5),(12);""",
+            {"table": table},
+        )
+    )
+
+    task = MapToNewColumns(
+        table=table,
+        select="SELECT id, val FROM {{table}}",
+        columns=[ValueColumn("doubled", "INT")],
+        fn=FnBuilder(double).pop_id_fields("id"),
+    )
+    task.render(DEFAULT_RENDERER)
+
+    task.run(conn)
+    assert get_rows(conn, table, order_by=[Identifier("id")]) == [
+        (1, 2, 4),
+        (2, 5, 10),
+        (3, 12, 24),
+    ]
+    task.delete(conn)
+    assert get_rows(conn, table, order_by=[Identifier("id")]) == [
+        (1, 2),
+        (2, 5),
+        (3, 12),
+    ]
+
+
+def test_map_columns_resumable(conn: PsycopgConn):
+    def failing(val: int):
+        if val < 10:
+            return {"doubled": val * 2}
+        else:
+            raise RuntimeError()
+
+    table = Table("test_map_columns_resumable")
+    conn.pg().execute(
+        DEFAULT_RENDERER.render(
+            """\
+            CREATE TABLE {{table}}(
+                id SERIAL PRIMARY KEY,
+                val INT
+            );
+            INSERT INTO {{table}}(val) VALUES
+            (2),(5),(12);""",
+            {"table": table},
+        )
+    )
+
+    task = MapToNewColumns(
+        table=table,
+        select="SELECT id, val FROM {{table}}",
+        columns=[ValueColumn("doubled", "INT")],
+        fn=FnBuilder(failing).pop_id_fields("id"),
+        is_done_column="__success",
+    )
+    task.render(DEFAULT_RENDERER)
+
+    with pytest.raises(RuntimeError):
+        task.run(conn)
+
+    assert get_rows(conn, table, order_by=[Identifier("id")]) == [
+        (1, 2, 4, True),
+        (2, 5, 10, True),
+        (3, 12, None, False),
+    ]
+    task.delete(conn)
+    assert get_rows(conn, table, order_by=[Identifier("id")]) == [
+        (1, 2),
+        (2, 5),
+        (3, 12),
+    ]
