49feb4e68453f6f889bb103559cf5fe4300b2b80
snorkysnark <snorkysnark@gmail.com>
2024-01-02 13:51:49 +0100

    remove typer support
    since it doesn't integrate well with pure click CLIs (for nested commands, etc)

--- ralsei/app.py
+++ ralsei/app.py
@@ -1,16 +1,10 @@
 import click
-import typer
 from rich import traceback
-from typing import Any, Callable, Self, Sequence, cast, overload
+from typing import Callable, Sequence, overload
 import itertools
 
 from ralsei.pipeline import Pipeline
 from ralsei.context import EngineContext
-
-
-POSITONAL_ARGS_ERROR = """\
-All pipeline constructor args must be annotated as Typer CLI Options.
-See: https://typer.tiangolo.com/tutorial/options/help/"""
 
 
 def get_existing_names(params: Sequence[click.Parameter]) -> tuple[set[str], set[str]]:
@@ -94,19 +88,6 @@ class Ralsei:
             self._custom_cli_options = custom_cli_options
 
         self.cli = build_cli(self._pipeline_constructor, self._custom_cli_options)
-
-    @classmethod
-    def from_typer(cls, typer_callable: Callable[..., Any]) -> Self:
-        typer_app = typer.Typer(add_help_option=False, add_completion=False)
-        typer_app.command()(typer_callable)
-        command = typer.main.get_command(typer_app)
-
-        assert command.callback, "Generated command is missing a callback"
-        for param in command.params:
-            if not isinstance(param, click.Option):
-                raise ValueError(POSITONAL_ARGS_ERROR)
-
-        return cls(command.callback, cast(Sequence[click.Option], command.params))
 
     def run(self):
         traceback.install(show_locals=True)
