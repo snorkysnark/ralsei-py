3b52a54c8a923e58d5bae176ce9bde6cb6698cb8
snorkysnark <snorkysnark@gmail.com>
2024-01-12 16:20:23 +0100

    document ralsei.task.base

--- ralsei/task/base.py
+++ ralsei/task/base.py
@@ -11,40 +11,59 @@ SqlLike = TextClause | list[TextClause]
 
 
 class Task(ABC):
+    """Task base class"""
+
     @abstractmethod
     def run(self, ctx: ConnectionContext):
-        ...
+        """Run the task"""
 
     @abstractmethod
     def delete(self, ctx: ConnectionContext):
-        ...
+        """Delete whatever :py:meth:`~run` has created"""
 
     def redo(self, ctx: ConnectionContext):
+        """Calls :py:meth:`~delete` and then :py:meth:`~run`"""
         self.delete(ctx)
         self.run(ctx)
 
     @abstractproperty
     def output(self) -> Any:
-        ...
+        """Object created or modified by this task
+        (usually a :py:class:`ralsei.types.Table`)
+
+        Used for resolving :py:meth:`ralsei.graph.Pipeline.outputof`
+        """
 
     @abstractmethod
     def exists(self, ctx: ConnectionContext) -> bool:
-        ...
+        """Check if task has already been done"""
 
     def sql_scripts(self) -> Iterable[tuple[str, SqlLike]]:
+        """
+        Returns:
+            pairs of (name, SQL statement)
+        """
+
         return []
 
 
 class TaskImpl(Task, Generic[T]):
+    """Task with a predefined constructor"""
+
     @abstractmethod
     def __init__(self, this: T, env: SqlalchemyEnvironment) -> None:
         ...
 
 
 class TaskDef:
+    """Holds constructor arguments for a task that will be instantiated later"""
+
     Impl: type[TaskImpl[Self]]
+    """The actual task class"""
 
     def create(self, env: SqlalchemyEnvironment) -> TaskImpl[Self]:
+        """Instantiate the task"""
+
         return self.Impl(self, env)
 
 
