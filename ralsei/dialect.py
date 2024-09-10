0b52030f39b400fbd5f32cd3edd82327d4b826d4
Evgenii Rechkalov <snorkysnark@gmail.com>
2024-09-10 20:23:25 +0200

    simplify dialect module

--- ralsei/dialect.py
+++ ralsei/dialect.py
@@ -1,21 +1,34 @@
-from __future__ import annotations
-from typing import Optional
+from typing import Callable
 
 from ralsei.sql_adapter import ToSql
 from ralsei.types import Sql
 
 
 class BaseDialectInfo:
-    name: str = "base"
     autoincrement_key: ToSql = Sql("SERIAL PRIMARY KEY")
     supports_column_if_not_exists: bool = True
     supports_rowcount: bool = True
 
 
+type DialectLike = BaseDialectInfo | type[BaseDialectInfo]
+
+dialect_registry: dict[str, DialectLike] = {}
+
+
+def register_dialect[T: DialectLike](driver: str) -> Callable[[T], T]:
+    def wrapper(dialect: T):
+        dialect_registry[driver] = dialect
+        return dialect
+
+    return wrapper
+
+
+@register_dialect("postgresql")
 class PostgresDialectInfo(BaseDialectInfo):
-    name = "postgres"
+    pass
 
 
+@register_dialect("sqlite")
 class SqliteDialectInfo(BaseDialectInfo):
     name = "sqlite"
     autoincrement_key = Sql("INTEGER PRIMARY KEY AUTOINCREMENT")
@@ -23,40 +36,9 @@ class SqliteDialectInfo(BaseDialectInfo):
     supports_rowcount = False
 
 
-class DialectNotFoundError(KeyError):
-    pass
-
-
-class DialectRegistry:
-    def __init__(self, by_name: Optional[dict[str, BaseDialectInfo]]) -> None:
-        self._by_name = by_name or {}
-
-    def register(self, dialect_name: str, dialect_info: BaseDialectInfo):
-        self._by_name[dialect_name] = dialect_info
-
-    def get_info(self, dialect_name: str) -> BaseDialectInfo:
-        dialect_info = self._by_name.get(dialect_name, None)
-        if not dialect_info:
-            raise DialectNotFoundError(dialect_name)
-
-        return dialect_info
-
-    def copy(self) -> DialectRegistry:
-        return DialectRegistry(self._by_name.copy())
-
-
-DEFAULT_DIALECT_REGISTRY = DialectRegistry(
-    {
-        "postgresql": PostgresDialectInfo(),
-        "sqlite": SqliteDialectInfo(),
-    }
-)
-
 __all__ = [
     "BaseDialectInfo",
     "PostgresDialectInfo",
     "SqliteDialectInfo",
-    "DialectNotFoundError",
-    "DialectRegistry",
-    "DEFAULT_DIALECT_REGISTRY",
+    "register_dialect",
 ]
