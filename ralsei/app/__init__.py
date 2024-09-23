e902424e24846c1f8b274034ce780a8c04b6c0ae
Evgenii Rechkalov <snorkysnark@gmail.com>
2024-09-23 16:41:27 +0200

    fix subcommand building
    + use sqlalchemy.URL parameter, not string

--- ralsei/app/__init__.py
+++ ralsei/app/__init__.py
@@ -1,18 +1,19 @@
 import sys
 import click
 from pathlib import Path
-from typing import Sequence
+from typing import Callable, Sequence
 from rich.console import Console
 from rich.prompt import Prompt
+import sqlalchemy
 
 from ralsei.graph import Pipeline, TreePath, TaskSequence
-from ralsei.connection import create_engine, ConnectionEnvironment
+from ralsei.connection import create_engine, ConnectionEnvironment, ConnectionExt
 from ralsei.task import ROW_CONTEXT_ATRRIBUTE
 from ralsei.jinja import SqlEnvironment
 from ralsei.dialect import get_dialect
 from ralsei.utils import expect
 
-from ._parsers import TYPE_TREEPATH
+from ._parsers import type_treepath, type_sqlalchemy_url
 from ._decorators import extend_params
 from ._rich import print_task_scripts
 from ._opener import open_in_default_app
@@ -30,9 +31,9 @@ def confirm_sequence(sequence: TaskSequence):
 
 
 class Ralsei:
-    def __init__(self, db: str, pipeline: Pipeline) -> None:
+    def __init__(self, url: sqlalchemy.URL, pipeline: Pipeline) -> None:
         self.pipeline = pipeline
-        self.engine = create_engine(db)
+        self.engine = create_engine(url)
 
         env = SqlEnvironment(get_dialect(self.engine.dialect.name))
         self._prepare_env(env)
@@ -54,49 +55,23 @@ class Ralsei:
     @classmethod
     def build_cli(cls) -> click.Group:
         @extend_params(getattr(cls, "__click_params__", []))
-        @click.option("--db", "-d", help="sqlalchemy database url", required=True)
+        @click.option(
+            "--db",
+            "-d",
+            type=type_sqlalchemy_url,
+            help="sqlalchemy database url",
+            required=True,
+        )
         @click.group(context_settings=dict(help_option_names=["-h", "--help"]))
         @click.pass_context
-        def cli(ctx: click.Context, db: str, *args, **kwargs):
+        def cli(ctx: click.Context, db: sqlalchemy.URL, *args, **kwargs):
             ctx.obj = cls(db, *args, **kwargs)
 
-        for name, action, ask in [
-            ("run", TaskSequence.run, False),
-            ("delete", TaskSequence.delete, True),
-            ("redo", TaskSequence.redo, True),
-        ]:
+        cls.__build_subcommand(cli, "run", TaskSequence.run)
+        cls.__build_subcommand(cli, "delete", TaskSequence.delete, ask=True)
+        cls.__build_subcommand(cli, "redo", TaskSequence.redo, ask=True)
 
-            @click.option(
-                "--from",
-                "from_filters",
-                help="run this task and its dependencies",
-                type=TYPE_TREEPATH,
-                multiple=True,
-            )
-            @click.option(
-                "--one",
-                "single_filters",
-                help="run only this task",
-                type=TYPE_TREEPATH,
-                multiple=True,
-            )
-            @cli.command(name)
-            @click.pass_context
-            def cmd(
-                ctx: click.Context,
-                from_filters: Sequence[TreePath],
-                single_filters: Sequence[TreePath],
-            ):
-                this = expect(
-                    ctx.find_object(Ralsei), RuntimeError("click context not set")
-                )
-
-                sequence = this.dag.sort_filtered(from_filters, single_filters)
-                if not ask or confirm_sequence(sequence):
-                    with this.connect() as conn:
-                        action(sequence, conn.sqlalchemy)
-
-        @click.argument("task_name", metavar="TASK", type=TYPE_TREEPATH)
+        @click.argument("task_name", metavar="TASK", type=type_treepath)
         @cli.command("describe")
         @click.pass_context
         def describe_cmd(ctx: click.Context, task_name: TreePath):
@@ -117,6 +92,43 @@ class Ralsei:
             open_in_default_app(rendered)
 
         return cli
+
+    @staticmethod
+    def __build_subcommand(
+        group: click.Group,
+        name: str,
+        action: Callable[[TaskSequence, ConnectionExt], None],
+        ask: bool = False,
+    ):
+        @click.option(
+            "--from",
+            "from_filters",
+            help="run this task and its dependencies",
+            type=type_treepath,
+            multiple=True,
+        )
+        @click.option(
+            "--one",
+            "single_filters",
+            help="run only this task",
+            type=type_treepath,
+            multiple=True,
+        )
+        @group.command(name)
+        @click.pass_context
+        def cmd(
+            ctx: click.Context,
+            from_filters: Sequence[TreePath],
+            single_filters: Sequence[TreePath],
+        ):
+            this = expect(
+                ctx.find_object(Ralsei), RuntimeError("click context not set")
+            )
+
+            sequence = this.dag.sort_filtered(from_filters, single_filters)
+            if not ask or confirm_sequence(sequence):
+                with this.connect() as conn:
+                    action(sequence, conn.sqlalchemy)
 
     @classmethod
     def run_cli(cls, *args, **kwargs):
